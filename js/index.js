fb435a2d4b57ad642abd6918e197df05456bc349
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-12 00:56:30 +0200

    added css Nav show justify-content

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const bubble = document.querySelectorAll(".bubble")
+const bubblebtn = document.querySelector(".bubble")
 const input = document.querySelector("#taskInput")
 const submit = document.querySelector("#submit")
 const ul = document.querySelector(".list-unstyled")
@@ -7,6 +8,9 @@ const checkbox = document.querySelector('input[type="checkbox"]')
 const progress = document.querySelector('.progress-bar')
 const wallpapers = "/src/Wallpaper/"
 
+const montain = document.querySelector('#Montain')
+const montain2 = document.querySelector('#Montain2')
+const forest = document.querySelector('#Forest')
 
 // const checked = document.querySelectorAll('input[type="checkbox"]:checked')
 
@@ -35,12 +39,23 @@ submit.addEventListener("click",add)
 
 // WALLPAPER
 const changeWallpaper = ()=>{
+if(bubble[0]){
     document.body.style.backgroundImage = "url('/src/Wallpaper/Montain.jpg')";
     document.body.style.filter = "grayscale(100%)"
     console.log("Hello")
+} else if(bubble[1]){
+    document.body.style.backgroundImage = "url('/src/Wallpaper/Montain2.jpg')";
+    document.body.style.filter = "grayscale(100%)"
+    console.log("Hello")
+}
 }
 
-bubble.addEventListener("click",changeWallpaper)
+
+bubble[0].addEventListener("click",()=> document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain.jpg')")
+bubble[1].addEventListener("click",()=> document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain_2.jpg')")
+bubble[2].addEventListener("click",()=> document.body.style.backgroundImage = "url('\/src/Wallpaper/Forest.jpg')")
+
+// bubble.addEventListener("click",changeWallpaper)
 //WALLPAPER END
 
 // CHECKBOX COUNTER
@@ -49,5 +64,3 @@ bubble.addEventListener("click",changeWallpaper)
 // console.log(checkbox)
 //  checkbox.addEventListener('change', console.log(checked.length))
 
-
-bubble.addEventListener("click",console.log(wallpapers))
