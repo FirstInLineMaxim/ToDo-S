37282c9aaf050d6e670fccfad7c437fa54933db1
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-11 17:15:34 +0200

    WALLPAPER

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 const bubble = document.querySelector(".bubble")
 const input = document.querySelector("#taskInput")
+const submit = document.querySelector("#submit")
 
 
+
+
+// WALLPAPER
 const changeWallpaper = ()=>{
     document.body.style.backgroundImage = "url('/src/Wallpaper/Montain.jpg')";
     console.log("Hello")
 }
 
-
 bubble.addEventListener("click",changeWallpaper)
-
+//WALLPAPER END
 
