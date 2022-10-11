058739668b7e5f3e6b90ffa0234b551abfbe3c48
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-11 17:17:59 +0200

    +li + task const

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const bubble = document.querySelector(".bubble")
 const input = document.querySelector("#taskInput")
 const submit = document.querySelector("#submit")
+const li = document.createElement("li")
+const task = document.createTextNode("test")
+
+
+li.appendChild(task);
 
 
 
