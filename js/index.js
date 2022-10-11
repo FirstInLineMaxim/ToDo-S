6b587194134ebe233c51bdbe94ce58bfe4ce70c0
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-11 18:26:29 +0200

    input.value

--- js/index.js
+++ js/index.js
@@ -3,15 +3,16 @@ const input = document.querySelector("#taskInput")
 const submit = document.querySelector("#submit")
 const ul = document.querySelector(".list-unstyled")
 const li = document.createElement("li")
-const task = []
-const checkbox = `<label class="">${task}
-<input type="checkbox" checked="checked">
-<span class="checkmark"></span>
-</label>`
-
+// const task = [];
+let checkbox ;
+console.log(task)
 const add = function (){
     if(input.value !== ""){
         task.push(input.value)
+        checkbox = `<label class="">
+        <input type="checkbox" checked="checked">
+        <span class="checkmark">${input.value}</span>
+        </label>`
     ul.innerHTML += `<li>${checkbox}</li>`
     input.value = ""
 } else {
