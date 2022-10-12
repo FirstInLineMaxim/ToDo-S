31a4b6083ac023ad5996ffa8ff6ee043d25e131d
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-12 17:49:50 +0200

    formating

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
 const bubble = document.querySelectorAll(".bubble")
-// const bubblebtn = document.querySelector(".bubble")
-const task = document.querySelector(".task") || [0];
 const input = document.querySelector("#taskInput")
 const submit = document.querySelector("#submit")
-const ul = document.querySelector(".list-unstyled")
-const li = document.createElement("li")
-const label = document.createElement("label")
-const checkmark = document.createElement("input")
 const checkbox = document.querySelector('input[type="checkbox"]')
 const progress = document.querySelector('.progress-bar')
 // WALLPAPER BUBBLES
 const montain = document.querySelector('#Montain')
 const montain2 = document.querySelector('#Montain2')
 const forest = document.querySelector('#Forest')
-
+//CREATE ELEMENTS
+const li = document.createElement("li")
+const label = document.createElement("label")
+const checkmark = document.createElement("input")
 // const checked = document.querySelectorAll('input[type="checkbox"]:checked')
 
 
@@ -22,28 +19,32 @@ const forest = document.querySelector('#Forest')
 // const add = function () {
 //     if (input.value !== "") {
 
-    //     task = `<label class="containerMark">       
-    //     <input type="checkbox" class="checkmark">
-    //     ${input.value}
-    //     </label>
-    //     <button onclick="Remove()">Click me to remove this </button>`
+//     task = `<label class="containerMark">       
+//     <input type="checkbox" class="checkmark">
+//     ${input.value}
+//     </label>
+//     <button onclick="Remove()">Click me to remove this </button>`
 
-    //     ul.innerHTML += `<li>${task}</li>`
-    //     input.value = ""
-    //     checked2++
-    //     document.querySelector('.progress-bar').style.width = checked2 + "%"// progressbar
-    // } else {
-    //     alert("Seems like you have done all")  //alert on no input
-    // }
+//     ul.innerHTML += `<li>${task}</li>`
+//     input.value = ""
+//     checked2++
+//     document.querySelector('.progress-bar').style.width = checked2 + "%"// progressbar
+// } else {
+//     alert("Seems like you have done all")  //alert on no input
+// }
 
 // }
 
 
 // ADDING THE TASK + CHECKBOX
 let checked2 = [0];
-const add = function (){
-    if (input.value !==""){
-        ul.innerHTML += `<li class="task">${input.value}</li>`
+const add = function () {
+    if (input.value !== "") {
+
+        ul.innerHTML += `<label class="containerMark">       
+        <li class="task"><input type="checkbox" class="checkmark">
+            ${input.value}</li>
+             </label>`
         input.value = ""
         checked2++
         document.querySelector('.progress-bar').style.width = checked2 + "%"// progressbar
@@ -53,7 +54,7 @@ const add = function (){
 }
 
 //SUBMIT ON ENTER KEY PRESS
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
     event.preventDefault();
     if (event.keyCode === 13) {
         submit.click();
@@ -62,7 +63,7 @@ input.addEventListener("keyup", function(event) {
 
 
 // DELETE
-function Remove (){
+function Remove() {
     ul.removeChild(li)
 }
 
@@ -75,4 +76,3 @@ submit.addEventListener("click", add)
 bubble[0].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain.jpg')")
 bubble[1].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain_2.jpg')")
 bubble[2].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Forest.jpg')")
-
