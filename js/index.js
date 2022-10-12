bbade7347ec6cf6b37121559c7d8649efc2d3c20
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-12 14:49:49 +0200

    Added Enter KEy

--- js/index.js
+++ js/index.js
@@ -7,60 +7,46 @@ const li = document.createElement("li")
 const checkbox = document.querySelector('input[type="checkbox"]')
 const progress = document.querySelector('.progress-bar')
 const wallpapers = "/src/Wallpaper/"
-
+// WALLPAPER BUBBLES
 const montain = document.querySelector('#Montain')
 const montain2 = document.querySelector('#Montain2')
 const forest = document.querySelector('#Forest')
 
 // const checked = document.querySelectorAll('input[type="checkbox"]:checked')
 
-let checked2 =[0];
-const add = function (){
-    if(input.value !== ""){
+
+// ADDING THE TASK + CHECKBOX
+let checked2 = [0];
+const add = function () {
+    if (input.value !== "") {
 
         task = `<label class="containerMark">       
         <input type="checkbox" class="checkmark">
-        <span>${input.value}</span>
+        ${input.value}
         </label>`
 
-    ul.innerHTML += `<li>${task}</li>`
-    input.value = ""
-    checked2 ++
-    document.querySelector('.progress-bar').style.width = checked2 + "%"// progressbar
-} else {
-    alert("Seems like you have done all")  //alert on no input
-}
+        ul.innerHTML += `<li>${task}</li>`
+        input.value = ""
+        checked2++
+        document.querySelector('.progress-bar').style.width = checked2 + "%"// progressbar
+    } else {
+        alert("Seems like you have done all")  //alert on no input
+    }
 
 }
 
-console.log(progress)
-
-submit.addEventListener("click",add)
-
-// WALLPAPER
-const changeWallpaper = ()=>{
-if(bubble[0]){
-    document.body.style.backgroundImage = "url('/src/Wallpaper/Montain.jpg')";
-    document.body.style.filter = "grayscale(100%)"
-    console.log("Hello")
-} else if(bubble[1]){
-    document.body.style.backgroundImage = "url('/src/Wallpaper/Montain2.jpg')";
-    document.body.style.filter = "grayscale(100%)"
-    console.log("Hello")
-}
-}
+//SUBMIT ON ENTER KEY PRESS
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        submit.click();
+    }
+});
 
 
-bubble[0].addEventListener("click",()=> document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain.jpg')")
-bubble[1].addEventListener("click",()=> document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain_2.jpg')")
-bubble[2].addEventListener("click",()=> document.body.style.backgroundImage = "url('\/src/Wallpaper/Forest.jpg')")
-
-// bubble.addEventListener("click",changeWallpaper)
-//WALLPAPER END
-
-// CHECKBOX COUNTER
 
 
-// console.log(checkbox)
-//  checkbox.addEventListener('change', console.log(checked.length))
-
+submit.addEventListener("click", add)
+bubble[0].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain.jpg')")
+bubble[1].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain_2.jpg')")
+bubble[2].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Forest.jpg')")
