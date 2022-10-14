cbf31a6e285f0d3099bdabf371773449cde668d7
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-10-14 15:27:05 +0200

    tooo much made this shit work

--- js/index.js
+++ js/index.js
@@ -16,28 +16,6 @@ const checkmark = document.createElement("input")
 // const checked = document.querySelectorAll('input[type="checkbox"]:checked')
 
 
-
-// let checked2 = [0];
-// const add = function () {
-//     if (input.value !== "") {
-
-//     task = `<label class="containerMark">       
-//     <input type="checkbox" class="checkmark">
-//     ${input.value}
-//     </label>
-//     <button onclick="Remove()">Click me to remove this </button>`
-
-//     ul.innerHTML += `<li>${task}</li>`
-//     input.value = ""
-//     checked2++
-//     document.querySelector('.progress-bar').style.width = checked2 + "%"// progressbar
-// } else {
-//     alert("Seems like you have done all")  //alert on no input
-// }
-
-// }
-
-
 // ADDING li with Inputed Task to ul
 const ul = document.querySelector(".list-unstyled")
 const handleCLick = () => {
@@ -48,23 +26,117 @@ const handleCLick = () => {
     task.value = `${input.value}`
     task.disabled = true
     task.className = "Tasks2"
+
     // creates the edit button 
     const edit = document.createElement("button")
     edit.innerText = "edit"
+    edit.className = "edit"
+
     //creates 
     const del = document.createElement("button")
     del.innerText = "del"
+    del.className = "del"
+
 
   ul.appendChild(li).appendChild(task)
   li.appendChild(edit)
   li.appendChild(del)
-  tasks = document.querySelectorAll(".Tasks2")
   input.value = ""
-//   listen = tasks.addEventListener("click",edit)
 
 };
 
+const taskList = document.querySelector(".list-unstyled") // 
+taskList.addEventListener("click",function(e){
+        //checks if uelement in ul is with class edit.
+    if (e.target.className === "edit"){
+        edit(e.target.previousSibling)
+        return 
+    } 
+    //checks if element in ul is with class save.
+    if (e.target.className === "save"){
+        edit(e.target.previousSibling)
+        return
+    }   
+    //checks if uelement un ul is with class del.
+    if (e.target.className === "del"){
+        Remove(e.target.parentElement)
+        return
+    }
+    //checks if uelement un ul is with class Task2.
+    // if(e.target.className === Task2){
+    //     //change class to something flex and bigger for conntent 
+    // }
+})
+
+
+// (e) => 
+// (e.target.className === "edit" && "save") ? edit(e.target.previousSibling) : console.log(e.target))
+
+
+
+
+
+//SUBMIT ON ENTER KEY PRESS
 submit.addEventListener('click', handleCLick)
+input.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        submit.click();
+    }
+});
+
+
+// DELETE
+function Remove(selectedLi) {
+    ul.removeChild(selectedLi)
+}
+
+// EDIT
+function edit (selectedInput){
+    if (selectedInput.disabled === true){
+        selectedInput.disabled = false;
+        //CHANGE PENCIL TO SAVE
+        selectedInput.nextSibling.className = "save"
+
+    } else {selectedInput.disabled = true
+        selectedInput.nextSibling.className = "edit"}
+
+}
+//COUNT CHECKED BOXES DISPLAY IN PROGRESS
+
+
+bubble[0].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain.jpg')")
+bubble[1].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain_2.jpg')")
+bubble[2].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Forest.jpg')")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -89,34 +161,3 @@ submit.addEventListener('click', handleCLick)
 //         alert("Seems like you have done all")  //alert on no input
 //     }
 // }
-
-//SUBMIT ON ENTER KEY PRESS
-input.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.keyCode === 13 || event.keyCode === 32) {
-        submit.click();
-    }
-});
-
-
-// DELETE
-function Remove() {
-    ul.removeChild(li)
-}
-
-// EDIT
-function edit (e){
-    
-    tasks[e].disabled = false
-    console.log("hello")
-}
-//COUNT CHECKED BOXES DISPLAY IN PROGRESS
-
-
-// submit.addEventListener("click", add)
-
-
-let listen = null;
-bubble[0].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain.jpg')")
-bubble[1].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Montain_2.jpg')")
-bubble[2].addEventListener("click", () => document.body.style.backgroundImage = "url('\/src/Wallpaper/Forest.jpg')")
